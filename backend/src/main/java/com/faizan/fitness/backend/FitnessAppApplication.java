88136dee8ce97f1276ff06dd538e3bfa3b47ac85
Faizan <faizanansari897@gmail.com>
2025-06-22 10:25:33 +0530

    initial commit: added Spring Boot backend with API and config

---
+++ backend/src/main/java/com/faizan/fitness/backend/FitnessAppApplication.java
@@ -0,0 +1,14 @@
+package com.faizan.fitness.backend;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/faizan/fitness/backend/FitnessAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faizan.fitness.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
